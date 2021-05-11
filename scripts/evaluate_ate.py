3662c0756174a784dc1b68de58033fcf7cba6e9f
samwen <samwen@umich.edu>
2021-05-11 00:27:53 -0400

    added more examples

--- scripts/evaluate_ate.py
+++ scripts/evaluate_ate.py
@@ -180,16 +180,16 @@ if __name__=="__main__":
         fig = plt.figure()
         ax = fig.add_subplot(111)
         plot_traj(ax,first_stamps,first_xyz_full.transpose().A,'-',"black","ground truth")
-        plot_traj(ax,second_stamps,second_xyz_full_aligned.transpose().A,'-',"blue","estimated")
+        plot_traj(ax,second_stamps,second_xyz_full.transpose().A,'-',"blue","estimated")
 
-        label="difference"
-        for (a,b),(x1,y1,z1),(x2,y2,z2) in zip(matches,first_xyz.transpose().A,second_xyz_aligned.transpose().A):
-            ax.plot([x1,x2],[y1,y2],'-',color="red",label=label)
-            label=""
+        # label="difference"
+        # for (a,b),(x1,y1,z1),(x2,y2,z2) in zip(matches,first_xyz.transpose().A,second_xyz_aligned.transpose().A):
+        #     ax.plot([x1,x2],[y1,y2],'-',color="red",label=label)
+        #     label=""
             
         ax.legend()
             
         ax.set_xlabel('x [m]')
-        ax.set_ylabel('y [m]')
+        ax.set_ylabel('z [m]')
         plt.savefig(args.plot,dpi=90)
         
